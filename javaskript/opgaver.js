6f1c78909f0de524fc63ff4b0271b9885f141b37
oskhav <oskarhavre@gmail.com>
2024-11-08 11:17:26 +0100

    lol

--- javaskript/opgaver.js
+++ javaskript/opgaver.js
@@ -1,19 +1,3 @@
-function erPrimtall(n) {
-    // Sjekker om tallet er mindre enn 2, som ikke er primtall
-    if (n < 2) {
-        return false;
-    }
-    
-    // Sjekker om tallet kan deles på noen tall fra 2 til kvadratroten av n
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) {
-            return false; // Tallet er delelig med et annet tall enn 1 og seg selv
-        }
-    }
-    
-    return true; // Tallet er et primtall
+function sendMail(){
+    window.open('mailto:oskarhavre@icloud.com');
 }
-
-// Test eksempler
-console.log(erPrimtall(7));   // true
-console.log(erPrimtall(10));  // false
