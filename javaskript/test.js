56e0cac18dccf8addb061052b178be93e1dd9d4f
oskhav <oskarhavre@gmail.com>
2024-11-15 11:25:23 +0100

    mail

--- javaskript/test.js
+++ javaskript/test.js
@@ -1,9 +1,34 @@
-let fruits = ["eple", "banan"];
+const robot = require('robotjs');
+const { exec } = require('child_process');
 
-console.log("Opprinnelig liste:", fruits);
+// Funksjon for å åpne en app
+function openApp(command) {
+    exec(command, (error) => {
+        if (error) {
+            console.error(`Kunne ikke åpne appen: ${error}`);
+            return;
+        }
+        console.log(`App åpnet: ${command}`);
+    });
+}
 
-fruits.push("appelsin");
-console.log("Etter push:", fruits);
+// Oppsett for tastetrykk
+function setupKeybindings() {
+    console.log("Trykk Ctrl+C for å avslutte programmet.");
 
-fruits.pop();
-console.log("Etter pop:", fruits);
+    // Sjekker tastetrykk kontinuerlig
+    setInterval(() => {
+        if (robot.keyToggle('control', 'down') && robot.keyTap('1')) {
+            openApp("notepad");  // Åpner Notepad i Windows
+        } else if (robot.keyToggle('control', 'down') && robot.keyTap('2')) {
+            openApp("calc");     // Åpner kalkulator i Windows
+        } else if (robot.keyToggle('control', 'down') && robot.keyTap('3')) {
+            openApp("mspaint");  // Åpner Paint i Windows
+        } else if (robot.keyToggle('control', 'down') && robot.keyTap('4')) {
+            openApp("explorer"); // Åpner File Explorer i Windows
+        }
+    }, 100);  // Sjekker hvert 100 millisekund
+}
+
+// Starter oppsettet
+setupKeybindings();
