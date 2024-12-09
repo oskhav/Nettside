16c8cc14ba8fde88743d2220d906307e7b2863f2
oskhav <oskarhavre@gmail.com>
2024-12-09 13:06:53 +0100

    log

--- javaskript/hallo.js
+++ javaskript/hallo.js
@@ -1,16 +1,17 @@
-console.log("hei verden");
-
-const name= "Oskar"
-console.log("hei" + name);
-
-let  a = 8
-let b = 16
-console.log(a + b);
-
-const fruits =["eple", "banan", "appelsin"]
-console.log(fruits[0]);
-console.log(fruits[fruits.length -1]);
 
 
+let a = [1, 2, 3, 4, 6]; // En array definert med noen tall
 
-    
+function kul(s) {
+    let b = ""; // Start med en tom streng for å bygge resultatet
+
+    // Iterer gjennom hvert andre element (hopp med 2 om gangen)
+    for (let i = 0; i < s.length; i += 2) {
+        b = b + s[i]; // Legg til hvert andre element i strengen b
+    }
+
+    return b; // Returner den oppbygde strengen
+}
+
+// Kall funksjonen med a som argument
+console.log(kul(a));
