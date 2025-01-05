7fcaaf37634645ba44f9e9c3b2f4493d8efc3fb5
oskhav <oskarhavre@gmail.com>
2025-01-05 20:27:00 +0100

    håp

--- javaskript/bmi.js
+++ javaskript/bmi.js
@@ -1,12 +1,13 @@
+// JavaScript-fil: bmi.js
 function calculateBMI() {
-    var weight = document.getElementById('weight').value;
-    var height = document.getElementById('height').value;
-  
-    if (weight > 0 && height > 0) {
-      var heightInMeters = height / 100;
-      var bmi = weight / (heightInMeters * heightInMeters);
-      document.getElementById('result').innerText = 'Your BMI is ' + bmi.toFixed(2);
-    } else {
-      document.getElementById('result').innerText = 'Please enter valid weight and height.';
-    }
+  var weight = document.getElementById('weight').value;
+  var height = document.getElementById('height').value;
+
+  if (weight > 0 && height > 0) {
+    var heightInMeters = height / 100;
+    var bmi = weight / (heightInMeters * heightInMeters);
+    document.getElementById('result').innerText = 'Din BMI er ' + bmi.toFixed(2);
+  } else {
+    document.getElementById('result').innerText = 'Vennligst skriv inn gyldig vekt og høyde.';
   }
+}
